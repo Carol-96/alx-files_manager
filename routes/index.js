b65e56d2ef99c1891060c10e342ed55900673198
JohnIanOngayi <ongayijohnian@gmail.com>
2024-10-14 10:09:31 +0300

    task 3: POST /users route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getStats, getStatus } from '../controllers/AppController';
+import addUser from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
+router.post('/users', addUser);
 
 export default router;
