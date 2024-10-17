3e367747874c527c39e56d04568376741769de3d
Carol-96 <karolynkalumu@gmail.com>
2024-10-17 20:11:52 +0000

    server

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,26 @@
-import { Router } from 'express';
-import { getStats, getStatus } from '../controllers/AppController';
-import addUser from '../controllers/UsersController';
-import {
-  getConnect,
-  getDisconnect,
-  getMe,
-} from '../controllers/AuthController';
-import {
-  getFile,
-  getIndex,
-  getShow,
-  postUpload,
-  putPublish,
-  putUnpublish,
-} from '../controllers/FilesController';
+// routes/index.js
 
-const router = Router();
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
-router.get('/status', getStatus);
-router.get('/stats', getStats);
-router.post('/users', addUser);
-router.get('/connect', getConnect);
-router.get('/disconnect', getDisconnect);
-router.get('/users/me', getMe);
-router.post('/files', postUpload);
-router.get('/files', getIndex);
-router.get('/files/:id', getShow);
-router.put('/files/:id/publish', putPublish);
-router.put('/files/:id/unpublish', putUnpublish);
-router.get('/files/:id/data', getFile);
+const router = express.Router();
 
-export default router;
+// Define routes
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);  // New endpoint for file data
+
+module.exports = router;
+
